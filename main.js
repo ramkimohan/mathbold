67ed84d57ba3538991c544d102ca9ecd470a4d4d
ramkimohan <ramkiram101098@gmail.com>
2024-07-24 12:58:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+ref="index.html"
+import resume from '../Resumepdf'
+
+var typed = new typed(".text",
+    {
+    strings:["Frontend Developer", "Web Developer", "Online Tutor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
